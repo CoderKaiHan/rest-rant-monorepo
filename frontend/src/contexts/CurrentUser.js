617b7fa6dafc2164683d100569ecb42712cb7090
CoderKaiHan <hankaicoder@gmail.com>
2024-04-25 18:52:18 -0400

    Fetch the current user on page load

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 
 export const CurrentUser = createContext()
@@ -7,6 +7,16 @@ function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null);
     window.setCurrentUser = setCurrentUser;
+    useEffect(() => {
+        
+        const getCurrentUser = async () => {
+            let response = await fetch('http://localhost:5000/authentication/profile')
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getCurrentUser()
+    },[])
+    
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
             {children}
